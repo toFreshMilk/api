4255c47f49a641f3a4cd7c52fda7951ba7d19be4
leesm <leesm@logisoft.co.kr>
2024-03-21 17:27:53 +0900

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bridgeLcsToDaeri',
+      script: './src/index.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      wait_ready: true,
+      kill_timeout: 5000,
+    },
+  ],
+}
