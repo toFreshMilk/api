651ae876056786f938ac8c7c0bf745e341346f9e
leesm <leesm@logisoft.co.kr>
2024-04-01 14:34:23 +0900

    v1 추가

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -5,10 +5,12 @@ const callcenter = require('./callcenter')
 const order = require('./order')
 const axiosInstance = require('../../common/axios')
 
-// /callcenter
 router.get('/price/estimate', async (req, res) => {
   const proc = await axiosInstance.sendRequest('Callcenter_Get_Dev', req.body)
-  res.json(proc)
+  // console.info(proc)
+  res.json({
+    body: proc.params || 'err',
+  })
 })
 
 router.use('/callcenter', callcenter)
