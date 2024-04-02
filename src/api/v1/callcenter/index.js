d356e7b64d9f31c002abffe460644ff920d2bf8e
leesm <leesm@logisoft.co.kr>
2024-04-02 12:19:47 +0900

    공통 try

--- src/api/v1/callcenter/index.js
+++ src/api/v1/callcenter/index.js
@@ -4,42 +4,24 @@ const axiosInstance = require('../../../common/axios')
 const router = express.Router()
 
 router.get('/:logiCallcenterCode', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('Callcenter_Get', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('Callcenter_Get', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 router.get('/:logiCallcenterCode/branches', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('CallcenterBranchs_Get', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('CallcenterBranchs_Get', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 router.post('/:logiCallcenterCode/branches', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('CallcenterBranchs_Post', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('CallcenterBranchs_Post', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 module.exports = router
--- src/api/v1/order/index.js
+++ src/api/v1/order/index.js
@@ -4,42 +4,24 @@ const axiosInstance = require('../../../common/axios')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('Order_Set_Registration', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('Order_Set_Registration', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 router.patch('/', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('Order_Set_Modification', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('Order_Set_Modification', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 router.delete('/', async (req, res) => {
-  let proc
-  try {
-    proc = await axiosInstance.sendRequest('Order_Set_Cancelation', req.body)
-  } catch (err) {
-    proc = String(err)
-  } finally {
-    res.json({
-      body: proc,
-    })
-  }
+  const { data } = await axiosInstance.sendRequest('Order_Set_Cancelation', req.body)
+  res.json({
+    body: data,
+  })
 })
 
 module.exports = router
